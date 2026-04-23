d7db9bc2b190f839392ef59b06e1421c3b769da2
erisep13 <139222741+erisep13@users.noreply.github.com>
2026-04-23 16:02:03 +0200

    Add files via upload

--- single-cell/seurat_integration_tool.R
+++ single-cell/seurat_integration_tool.R
@@ -11,25 +11,10 @@ seurat_object1 <- readRDS("seurat_object1.rds")
 seurat_object2 <- readRDS("seurat_object2.rds")
 
 
-# Previous step if you are running the integration upon objects created from previous integrations
-
-# Set default assay to RNA
-DefaultAssay(seurat_object1) <- "RNA"
-DefaultAssay(seurat_object2) <- "RNA"
-
-# Split the dataset and create a list with one dataset per sample
-seurat_object1_list <- SplitObject(seurat_object1, split.by = "orig.ident")
-seurat_object2_list <- SplitObject(seurat_object2, split.by = "orig.ident")
-
-# List of the Seurat objects
-seurat_object_list <- list(seurat_object1_list, seurat_object2_list)
-
-# SCTransform the datasets of the list
-seurat_object_list <- lapply(X = seurat_object_list, FUN = SCTransform, method = "glmGamPoi",  
-                             vars.to.regress = c("S.Score", "G2M.Score","Percent_mt"))
-
-
 # Seurat anchor based integration
+
+# List the datasets that are going to be integrated
+seurat_object_list <- list(seurat_object1, seurat_object2)
 
 # Selection of the integration features
 features <- SelectIntegrationFeatures(object.list = seurat_object_list)
@@ -55,3 +40,31 @@ DefaultAssay(integrated_object) <- "SCT" # or RNA
 
 # The new dataset must be preprocessed, clusterd and annotated as the individual objects
 
+
+# Previous additional step if you are running the integration upon objects created from previous integrations
+
+# Set default assay to RNA
+DefaultAssay(seurat_object1) <- "RNA"
+DefaultAssay(seurat_object2) <- "RNA"
+
+# Split the dataset and create a list with one dataset per sample
+seurat_object1_list <- SplitObject(seurat_object1, split.by = "orig.ident")
+seurat_object2_list <- SplitObject(seurat_object2, split.by = "orig.ident")
+
+# List of the Seurat objects
+seurat_object_list <- c(seurat_object1_list, seurat_object2_list)
+
+# SCTransform the datasets of the list
+seurat_object_list <- lapply(X = seurat_object_list, FUN = SCTransform, method = "glmGamPoi",  
+                             vars.to.regress = c("S.Score", "G2M.Score","Percent_mt"))
+
+# THIS CAN RISE BATCH EFFECT PROBLEMS
+
+# For integration of previously integrated datasets we can try simply merging the datasets, it usually does not rise batch effect
+DefaultAssay(integrated_seurat_object1) <- "RNA"
+DefaultAssay(integrated_seurat_object2) <- "RNA"
+
+integrated_object <- merge(integrated_seurat_object1, integrated_seurat_object2)
+
+
+# Preprocessing, clustering and annotations
